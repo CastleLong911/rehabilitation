91b83cb698ae223cc189b2d70795ada60531315b
CastleLong911 <castlelong911@gmail.com>
2025-05-19 18:01:56 +0900

    25/05/19 18:02

---
+++ silverIII/BOJ_1021.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+q = [i for i in range(1, n+1)]
+pos = 0
+result = 0
+for i in nums:
+    idx = q.index(i)
+    idx_e = len(q) - idx - 1 if len(q) - idx < idx else idx
+    pos_e = len(q) - pos - 1 if len(q) - pos < pos else pos
+    if abs(pos - idx) > idx_e + pos_e + 1:
+        result += idx_e + pos_e + 1
+    else:
+        result += abs(pos - idx)
+    q.remove(i)
+    pos = idx
+    if pos == len(q):
+        pos -= 1
+print(result)
--- silverV/test.py
+++ silverV/test.py
@@ -1,13 +1,2 @@
-import operator
-
-views = {
-    "alice": 120,
-    "bob": 150,
-    "carol": 110,
-    "dave": 300,
-    "erin": 200
-}
-v = iter(sorted(views.items(), key=operator.itemgetter(1), reverse=True))
-
-for i in range(3):
-    print(next(v)[0])
+s = "Python은 강력하다"
+print(''.join(reversed(s)))
